d3cfa39013c14622770c21cffd29c003efbaf374
gomidee <89821411+gomidee@users.noreply.github.com>
2021-12-19 10:47:50 +1000

    palmeira1teste

--- palmtree.sh
+++ palmtree.sh
@@ -39,9 +39,9 @@ if [[ $input == "Y" || $input == "y" ]]; then
 
 		   #Allow IP Fowarding
 
-		echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
-                echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
-                sudo sysctl -p /etc/sysctl.conf
+		echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
+                echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
+                sysctl -p /etc/sysctl.conf
 
                   #Add UFW allow rules
 		  ufw allow 41641/udp
@@ -68,8 +68,8 @@ read input
   if [[ $input == "Y" || $input == "y" ]]; then
                 
                 #Default Rules
-                sudo ufw default deny incoming
-		sudo ufw default allow outgoing
+                ufw default deny incoming
+		ufw default allow outgoing
 
 		echo "OK"
 
@@ -94,11 +94,104 @@ read input
 
   fi
 
+
   #Block ping requests
 sysctl -w net.ipv4.icmp_echo_ignore_all=1
 
+cp /etc/ufw/before.rules /etc/ufw/before.rules.backup
+rm /etc/ufw/before.rules
+
+cat << "EOF" >> /etc/ufw/before.rules
+#
+# rules.before
+#
+# Rules that should be run before the ufw command line added rules. Custom
+# rules should be added to one of these chains:
+#   ufw-before-input
+#   ufw-before-output
+#   ufw-before-forward
+#
+
+# Don't delete these required lines, otherwise there will be errors
+*filter
+:ufw-before-input - [0:0]
+:ufw-before-output - [0:0]
+:ufw-before-forward - [0:0]
+:ufw-not-local - [0:0]
+# End required lines
+
+
+# allow all on loopback
+-A ufw-before-input -i lo -j ACCEPT
+-A ufw-before-output -o lo -j ACCEPT
+
+# quickly process packets for which we already have a connection
+-A ufw-before-input -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+-A ufw-before-output -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+-A ufw-before-forward -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# drop INVALID packets (logs these in loglevel medium and higher)
+-A ufw-before-input -m conntrack --ctstate INVALID -j ufw-logging-deny
+-A ufw-before-input -m conntrack --ctstate INVALID -j DROP
+
+# ok icmp codes for INPUT
+-A ufw-before-input -p icmp --icmp-type echo-request -j DROP
+-A ufw-before-input -p icmp --icmp-type destination-unreachable -j ACCEPT
+-A ufw-before-input -p icmp --icmp-type time-exceeded -j ACCEPT
+-A ufw-before-input -p icmp --icmp-type parameter-problem -j ACCEPT
+-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT
+
+# ok icmp code for FORWARD
+-A ufw-before-forward -p icmp --icmp-type destination-unreachable -j ACCEPT
+-A ufw-before-forward -p icmp --icmp-type time-exceeded -j ACCEPT
+-A ufw-before-forward -p icmp --icmp-type parameter-problem -j ACCEPT
+-A ufw-before-forward -p icmp --icmp-type echo-request -j ACCEPT
+
+# allow dhcp client to work
+-A ufw-before-input -p udp --sport 67 --dport 68 -j ACCEPT
+
+#
+# ufw-not-local
+#
+-A ufw-before-input -j ufw-not-local
+
+# if LOCAL, RETURN
+-A ufw-not-local -m addrtype --dst-type LOCAL -j RETURN
+
+# if MULTICAST, RETURN
+-A ufw-not-local -m addrtype --dst-type MULTICAST -j RETURN
+
+# if BROADCAST, RETURN
+-A ufw-not-local -m addrtype --dst-type BROADCAST -j RETURN
+
+# all other non-local packets are dropped
+-A ufw-not-local -m limit --limit 3/min --limit-burst 10 -j ufw-logging-deny
+-A ufw-not-local -j DROP
+
+# allow MULTICAST mDNS for service discovery (be sure the MULTICAST line above
+# is uncommented)
+-A ufw-before-input -p udp -d 224.0.0.251 --dport 5353 -j ACCEPT
+
+# allow MULTICAST UPnP for service discovery (be sure the MULTICAST line above
+# is uncommented)
+-A ufw-before-input -p udp -d 239.255.255.250 --dport 1900 -j ACCEPT
+
+# don't delete the 'COMMIT' line or these rules won't be processed
+COMMIT
+EOF
+
+cp /etc/sysctl.conf /etc/sysctl.conf.backup
+cat << "EOF" >> /etc/sysctl.conf
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+sysctl -p
+
+ufw reload
+
 #To unblock ping requests, use following command
-  #sudo sysctl -w net.ipv4.icmp_echo_ignore_all=0
+  # sysctl -w net.ipv4.icmp_echo_ignore_all=0
 
 #Change default SSH Port
 
